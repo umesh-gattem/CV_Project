193fa8fae23b1a4dffd66543a4f2adfdd6e0a003
ugattem <ugattem@iu.edu>
2023-04-24 22:14:45 -0400

    Added Inception-ResNet model

---
+++ inception_resnet/inception_resnet_model.py
@@ -0,0 +1,186 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import RandomFlip, RandomRotation
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import json
+
+print(tf.__version__)
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+input_directory = "../yolo_predictions"
+train_dataset = image_dataset_from_directory(input_directory,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE,
+                                             validation_split=0.2,
+                                             subset='training',
+                                             label_mode="categorical",
+                                             seed=42)
+validation_dataset = image_dataset_from_directory(input_directory,
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE,
+                                                  image_size=IMG_SIZE,
+                                                  validation_split=0.2,
+                                                  subset='validation',
+                                                  label_mode="categorical",
+                                                  seed=42)
+validation_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take((2 * validation_batches) // 3)
+validation_dataset = validation_dataset.skip((2 * validation_batches) // 3)
+
+class_names = train_dataset.class_names
+print(class_names)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+
+
+def data_augmenter():
+    '''
+    Create a Sequential model composed of 2 layers
+    Returns:
+        tf.keras.Sequential
+    '''
+    data_augmentation = tf.keras.Sequential()
+    data_augmentation.add(RandomFlip(mode='horizontal'))
+    data_augmentation.add(RandomRotation(0.2))
+
+    return data_augmentation
+
+
+data_augmentation = data_augmenter()
+
+preprocess_input = tf.keras.applications.inception_resnet_v2.preprocess_input
+
+
+def image_claasification(image_shape=IMG_SIZE, data_augmentation=data_augmenter()):
+    ''' Define a tf.keras model for binary classification out of the MobileNetV2 model
+    Arguments:
+        image_shape -- Image width and height
+        data_augmentation -- data augmentation function
+    Returns:
+    Returns:
+        tf.keras.model
+    '''
+
+    input_shape = image_shape + (3,)
+
+    base_model = tf.keras.applications.InceptionResNetV2(input_shape=input_shape,
+                                                         include_top=False,  # <== Important!!!!
+                                                         weights='imagenet')  # From imageNet
+
+    # freeze the base model by making it non trainable
+    base_model.trainable = False
+
+    # create the input layer (Same as the imageNetv2 input size)
+    inputs = tf.keras.Input(shape=input_shape)
+
+    # apply data augmentation to the inputs
+    x = data_augmentation(inputs)
+
+    # data preprocessing using the same weights the model was trained on
+    x = preprocess_input(x)
+
+    # set training to False to avoid keeping track of statistics in the batch norm layer
+    x = base_model(x, training=False)
+
+    # add the new Binary classification layers
+    # use global avg pooling to summarize the info in each channel
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    # include dropout with probability of 0.2 to avoid overfitting
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    # use a prediction layer with one neuron (as a binary classifier only needs one)
+    outputs = tf.keras.layers.Dense(len(class_names), activation='softmax')(x)
+
+    model = tf.keras.Model(inputs, outputs)
+
+    return model
+
+
+model = image_claasification(IMG_SIZE, data_augmentation)
+
+base_learning_rate = 0.001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+
+initial_epochs = 5
+history = model.fit(train_dataset, validation_data=validation_dataset, epochs=initial_epochs)
+
+acc = [0.] + history.history['accuracy']
+val_acc = [0.] + history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+base_model = model.layers[4]
+base_model.trainable = True
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 120
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+# Define a BinaryCrossentropy loss function. Use from_logits=True
+loss_function = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+# Define an Adam optimizer with a learning rate of 0.1 * base_learning_rate
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1 * base_learning_rate)
+# Use accuracy as evaluation metric
+metrics = ['accuracy']
+
+model.compile(loss=loss_function, optimizer=optimizer, metrics=metrics)
+
+fine_tune_epochs = 5
+total_epochs = initial_epochs + fine_tune_epochs
+
+history_fine = model.fit(train_dataset,
+                         epochs=total_epochs,
+                         initial_epoch=history.epoch[-1],
+                         validation_data=validation_dataset)
+
+acc += history_fine.history['accuracy']
+val_acc += history_fine.history['val_accuracy']
+
+loss += history_fine.history['loss']
+val_loss += history_fine.history['val_loss']
+
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Valid Accuracy')
+plt.legend(loc="lower right")
+plt.savefig('accuracy.png')
+
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Valid Loss')
+plt.legend(loc="lower right")
+plt.savefig('loss.png')
+
+model.save("model_output/image_classify")
+
+with open('history.json', 'w') as f:
+    json.dump(history.history, f)
+
+with open('history1.json', 'w') as f:
+    json.dump(history_fine.history, f)
+## Evaluation:
+
+print(model.evaluate(test_dataset))
+
+## Prediction
+# image_batch, label_batch = next(iter(test_dataset))
+# outputs = model.predict(image_batch)
+# result = []
+# for i in outputs:
+#     if i[0] < 0.5:
+#         result.append(0)
+#     else:
+#         result.append(1)
+# print(result)
+# print(label_batch)
+
+
+# [0.2613685727119446, 0.921875]
